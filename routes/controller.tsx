1eedff67440111f84321bc7dcb961f9644488433
Brandao-lang <elijahbrandao@gmail.com>
2021-11-16 11:59:38 -0500

    server updates

--- routes/controller.tsx
+++ routes/controller.tsx
@@ -170,6 +170,10 @@ module.exports = {
     
             const user = await userCol.findOne({
                 email
+            }).then(res => 
+                console.log(res)
+            ).catch (err => {
+                return err
             })
 
             const verify = bcrypt.compareSync(password, user.password)
